b5bee4243daef0396d84c70f07ca8ebfcd4a7427
Ricardo Rossi <rrossi@endata.com>
2016-06-07 11:43:00 -0500

    added eslint and nightmare support

--- jobs/fetch-site.js
+++ jobs/fetch-site.js
@@ -1,3 +1,8 @@
+var Nightmare = require('nightmare');
+var nightmare = Nightmare({
+  show: true
+})
+
 /**
  * Fetch Site Job
  */
@@ -29,6 +34,28 @@ class FetchSite {
    * fetches site links
    */
   fetchSiteLinks(settings) {
+
+    nightmare
+      .goto('http://localhost:3000/')
+      .wait(function() {
+        return document.querySelectorAll('a[href^="/job"').length >= 3;
+      })
+      .evaluate(function() {
+        var hrefs = [];
+        var links = document.querySelectorAll('a[href^="/job"');
+        for (i = 0; i < links.length; i++) {
+          hrefs.push(links[i].href);
+        }
+        return hrefs;
+      })
+      .end()
+      .then(function(result) {
+        console.log(result);
+      })
+      .catch(function(error) {
+        console.error('Failed to fetch site:', error);
+      });
+
     console.log(settings);
     console.log('FETCHING LINKS');
     console.log(this.Q.INBOX);
